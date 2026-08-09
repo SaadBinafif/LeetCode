cb56bf92f76b0ab1692f102d09b98656e44aa1c0
Saad Binafif <136352567+SaadBinafif@users.noreply.github.com>
2026-08-09 21:50:34 +0530

    Time: 4 ms (100%), Space: 46.2 MB (16.71%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,16 +4,17 @@ class Solution {
         int reverse = 0;
         int original = x;
 
-        while(x > 0)
+        if(x < 0){
+            return false;
+        }
+
+        while(x != 0)
         {
             int digit = x%10;
             reverse = reverse*10 + digit;
             x = x/10;
-        }    
-
-        if(original == reverse)
-            return true;
+        }  
         
-        return false;
+        return original == reverse;
     }
 }
